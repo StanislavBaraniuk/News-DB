fe5c60728d2ec98e9ce7782a1fc921ff1f05f47d
stanislaw <stanislavbaraniuk@gmail.com>
2018-04-22 17:42:17 +0300

    fix

--- NewsDB/src/newsdb/Controller.java
+++ NewsDB/src/newsdb/Controller.java
@@ -17,11 +17,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  *
